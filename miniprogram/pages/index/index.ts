db0307ab780442a8d50f9e1f8e72bae2726cc0cd
NanCunChild <nancunchild@gmail.com>
2023-09-01 23:56:53 +0800

    style: index page style beautified.

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -3,19 +3,21 @@ Page({
     
   },
   onLoad() {
-    wx.showModal({
-      title: 'V1.1更新',
-      content: '本次开发修复了运动路程在整数时不显示两位小数的问题；添加了快速随机值的方式；加入了自定义路线。 ————NanCunChild',
-    })
+
   },
+
   GotoHUAWEIHealth:function(){
     wx.navigateTo({
       url:"/pages/HUAWEI/HUAWEI_Health"
     })
+    return;
   },
+
   GotoKeep:function(){
     wx.navigateTo({
       url:"/pages/Keep/Keep"
     })
-  }
+    return;
+  },
+
 })
